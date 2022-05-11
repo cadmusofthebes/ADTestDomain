27d11e9813902fb00207e60fab56c190bb1a349b
cadmusofthebes <45128341+cadmusofthebes@users.noreply.github.com>
2022-05-11 17:53:54 -0400

    Fixed tabs vs spaces

--- promoteToDC.ps1
+++ promoteToDC.ps1
@@ -3,11 +3,11 @@
 This script will automatically promote a Server 2016 or Server 2019 machine to a domain controller and create a domain
 
 .PARAMETER domain
-The domain parameter will be the name of your domain
+The domain parameter will be the name of your domain (Required)
 .PARAMETER disable
-This will disable the automatic start of the server tools when signing in as an admin
+This will disable the automatic start of the server tools when signing in as an admin (Optional)
 .PARAMETER static
-This will set a static IP address for the server
+This will set a static IP address for the server (Optional)
 
 .EXAMPLE
 ./promoteToDC.ps1 -domain matrix
@@ -25,12 +25,12 @@ Param(
     [switch]$help,
     [switch]$disable,
     [string]$static,
-	[string]$gateway,
+    [string]$gateway,
     [string]$domain
 )
 
 function checkAdmin(){
-	if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
         [Security.Principal.WindowsBuiltInRole] "Administrator")){
         Write-Host "[!] ERROR: You are not running this script as an administrator" -ForegroundColor Red
         Break
@@ -41,30 +41,30 @@ function checkAdmin(){
 function setStaticIP(){
     $static = [string]$static
     $gateway = [string]$gateway
-	$cidr = "24"
-	$dns = "127.0.0.1"
-	$dns2 = "1.1.1.1"
-	$adapter = (Get-NetAdapter).InterfaceIndex
-	$ipType = "IPv4"
-	$ipRegEx="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
-	if($static -notmatch $ipRegEx){
-		Write-Host "[!] ERROR: Static IP address is invalid" -ForegroundColor Red
-		exit
-	}
-	elseif($gateway -notmatch $ipRegEx){
-		Write-Host "[!] ERROR: Gateway IP address is invalid" -ForegroundColor Red
-		exit
-	}
-	else{
-		Write-Host "[*] Setting up requested static IP"
-		New-NetIPAddress -IPAddress $static -PrefixLength $cidr -InterfaceIndex $adapter -DefaultGateway $gateway -AddressFamily $ipType | Out-Null
-		Set-DNSClientServerAddress –InterfaceIndex $adapter –ServerAddresses ($dns,$dns2) | Out-Null
-	}
+    $cidr = "24"
+    $dns = "127.0.0.1"
+    $dns2 = "1.1.1.1"
+    $adapter = (Get-NetAdapter).InterfaceIndex
+    $ipType = "IPv4"
+    $ipRegEx="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+    if($static -notmatch $ipRegEx){
+        Write-Host "[!] ERROR: Static IP address is invalid" -ForegroundColor Red
+        exit
+    }
+    elseif($gateway -notmatch $ipRegEx){
+        Write-Host "[!] ERROR: Gateway IP address is invalid" -ForegroundColor Red
+        exit
+    }
+    else{
+        Write-Host "[*] Setting up requested static IP"
+        New-NetIPAddress -IPAddress $static -PrefixLength $cidr -InterfaceIndex $adapter -DefaultGateway $gateway -AddressFamily $ipType | Out-Null
+        Set-DNSClientServerAddress –InterfaceIndex $adapter –ServerAddresses ($dns,$dns2) | Out-Null
+    }
 }
 
 
 function disableServerTools(){
-	# Disable the loading of the server manager tool automatically at login
+    # Disable the loading of the server manager tool automatically at login
     Write-Host "[*] Disabling automatic startup of Server Manager"
     Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask | Out-Null
 }
@@ -75,8 +75,8 @@ function promoteDC(){
     $WarningPreference = 'SilentlyContinue'
 
     # Remind user to take a snapshot before running
-	Write-Host ""
-	Write-Host "[*] DO NOT CONTINUE UNLESS YOU HAVE TAKEN A SNAPSHOT!"  -ForegroundColor Red
+    Write-Host ""
+    Write-Host "[*] DO NOT CONTINUE UNLESS YOU HAVE TAKEN A SNAPSHOT!"  -ForegroundColor Red
     Read-Host "Press ENTER to continue..."
 
     <# Sets the DSRM password according to default group policy requirements.
@@ -101,7 +101,7 @@ function help(){
     Write-Host "[*] Setup Domain: ./$scriptName -domain matrix"
     Write-Host "[*] Disable Server Manager Auto-start: ./$scriptName -domain matrix -disable"
     Write-Host "[*] Setup Static IP: ./$scriptName -domain matrix -static 192.168.1.2 -gateway 192.168.1.1"
-	exit
+    exit
 }
 
 
@@ -109,23 +109,25 @@ if ($help){
     help
 }
 elseif ([string]::IsNullOrEmpty($domain)){
-	Write-Host "[!] ERROR: Required parameter '-domain' missing" -ForegroundColor Red
+    # TODO: Better error handling for when -domain is passed but there is no value
+    Write-Host "[!] ERROR: Required parameter '-domain' missing" -ForegroundColor Red
     help
 }
 else{
-	checkAdmin
-	if ($disable){
-		disableServerTools
-	}
-	if ($static){
-		if ([string]::IsNullOrEmpty($gateway)){
-			Write-Host "[!] ERROR: '-static' entered but no '-gateway' given" -ForegroundColor Red
-			help
-		}
-		else{
-			setStaticIP($static, $gateway)
-		}
-	}
+    checkAdmin
+    if ($disable){
+        disableServerTools
+    }
+    if ($static){
+        # TODO: Better error handling for when -static is passed but there is no value
+        if ([string]::IsNullOrEmpty($gateway)){
+            Write-Host "[!] ERROR: '-static' entered but no '-gateway' given" -ForegroundColor Red
+            help
+        }
+        else{
+            setStaticIP($static, $gateway)
+        }
+    }
     promoteDC
 
 }
