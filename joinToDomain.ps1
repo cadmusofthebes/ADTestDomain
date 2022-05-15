49879b3cf2dc8b1f2089a13db1809a9dbe25f4de
cadmusofthebes <45128341+cadmusofthebes@users.noreply.github.com>
2022-05-15 13:06:09 -0400

    Added menu for available adapters

--- joinToDomain.ps1
+++ joinToDomain.ps1
@@ -4,15 +4,13 @@ This script will automatically join a Windows 10 machine to a previously configu
 
 .PARAMETER domain
 The domain parameter will be the name of the domain you are attempting to join
-
 .PARAMETER user
 The user parameter is the name of a user who has domain admin rights
-
-.PARAMETER dns
-The dns parameter is the ip address of the domain controller
+.PARAMETER dc
+The dc parameter is the ip address of the domain controller
 
 .EXAMPLE
-joinToDomain.ps1 -domain matrix -user neo -dns 192.168.10.1
+joinToDomain.ps1 -domain matrix -user neo -dc 192.168.10.1
 #> 
 
 [CmdletBinding()]
@@ -20,34 +18,72 @@ Param(
     [switch]$help,
     [string]$domain,
     [string]$user,
-    [string]$dns
+    [string]$dc
 )
 
 
-# Set the DNS Server to that of the Domain Controller
+# Validate script is being run as an administrator
+function checkAdmin(){
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+        [Security.Principal.WindowsBuiltInRole] "Administrator")){
+        Write-Host "[!] ERROR: You are not running this script as an administrator" -ForegroundColor Red
+        Break
+    }
+}
+
+
+# Display list of interfaces and set the DNS Server to that of the Domain Controller
 function setDNS(){
     $dns = [string]$dns
-    $adapter = (Get-NetAdapter).InterfaceIndex
+
+    # Build interface menu
+    $adapters = Get-NetAdapter | Select ifIndex, Name
+    Write-Host "[*] Displaying list of interfaces available to modify DNS on`n"
+    Write-Host "================"
+    $menu = @{}
+    for ($i=1; $i -le ($adapters | Measure-Object).Count; $i++){
+        Write-Host "$i. $($adapters[$i-1].name)"
+        $menu.Add($i,($adapters[$i-1].ifIndex))
+    }
+    # Prompt user to select interface
+    Write-Host "================`n"
+    [int]$selection = Read-Host "[+] Choose which adapter to update"
+    while ($selection -lt ($adapters | Measure-Object).Count -or $selection -gt ($adapters | Measure-Object).Count){
+        Write-Host "[!] ERROR: Selection is invalid"
+        [int]$selection = Read-Host "[+] Choose which adapter to update"
+    }
+    # Assign requested interface
+    $adapter = $menu.Item($selection)
+
+    # Validate format of given DNS server
     $ipRegEx="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
-    if ($dns -notmatch $ipRegEx){
+    if ($dc -notmatch $ipRegEx){
         Write-Host "[!] ERROR: Domain Controller IP address is in an invalid format" -ForegroundColor Red
         exit
     }
+    # Assign given DNS address to selected interface
     else{
-        Write-Host "[*] Setting the DNS server to the IP address of the domain controller"
-        Set-DNSClientServerAddress –InterfaceIndex $adapter –ServerAddresses $dns | Out-Null
+        Write-Host "[*] Setting the DNS server to $dc"
+        Set-DNSClientServerAddress –InterfaceIndex $adapter –ServerAddresses $dc | Out-Null
     }
 }
 
 
 # Join the computer to the domain
 function joinDomain(){
-    Write-Host "[*] DO NOT CONTINUE UNLESS YOU HAVE RENAMED YOUR MACHINE AND TAKEN A SNAPSHOT!"  -ForegroundColor Red
+    Write-Host "[*] DO NOT CONTINUE UNLESS TAKEN A SNAPSHOT!"  -ForegroundColor Red
+    Write-Host "[*] The computer will automatically reboot when this is completed"
     Read-Host "Press ENTER to continue..."
     
     $password = Read-Host -Prompt "Enter password for $user" -AsSecureString
-    $credential = New-Object System.Management.Automation.PSCredential($user,$password) 
-    Add-Computer -domainname $domain -Credential $credential
+    $credential = New-Object System.Management.Automation.PSCredential($user,$password)
+    try{
+        Write-Host "[*] Attempting to join domain $domain on domain controller $dc"
+        Add-Computer -domainname $domain -Credential $credential -ErrorAction Stop | Out-null
+    }
+    catch{
+        Write-Host "[!] ERROR: Failed to add computer to domain. Validate that you can connect to the domain controller and the password is correct" -ForegroundColor Red
+    }
 }
 
 
@@ -55,8 +91,8 @@ function joinDomain(){
 function help(){
     $scriptName = split-path $MyInvocation.PSCommandPath -Leaf
     Write-Host "[*] This script will automatically join a Windows 10 machine to a previously configured domain"
-    Write-Host "[*] Usage: ./$scriptName -domain <existing domain> -user <domain admin> -dns <IP of domain controller>"
-    Write-Host "[*] Example: ./$scriptName -domain matrix -user neo -dns 192.168.10.1"
+    Write-Host "[*] Usage: ./$scriptName -domain <existing domain> -user <domain admin> -dc <IP of domain controller>"
+    Write-Host "[*] Example: ./$scriptName -domain matrix -user neo -dc 192.168.10.1"
 }
 
 
@@ -64,11 +100,12 @@ function help(){
 if ($help){
     help
 }
-elseif ([string]::IsNullOrEmpty($domain) -or [string]::IsNullOrEmpty($user) -or [string]::IsNullOrEmpty($dns)){
+elseif ([string]::IsNullOrEmpty($domain) -or [string]::IsNullOrEmpty($user) -or [string]::IsNullOrEmpty($dc)){
     Write-Host "[!] ERROR: Required parameter is missing" -ForegroundColor Red
     help
 }
 else{
+    checkAdmin
     setDNS
     joinDomain
 }
--- promoteToDC.ps1
+++ promoteToDC.ps1
@@ -17,7 +17,6 @@ This will set a static IP address for the server (Optional)
 
 .EXAMPLE
 ./promoteToDC.ps1 -domain matrix -static 192.168.1.2 -gateway 192.168.1.1
-
 #> 
 
 [CmdletBinding()]
@@ -40,15 +39,36 @@ function checkAdmin(){
 }
 
 
-# Assign a static IP to the primary interface
+# Display list of interfaces and assign static IP to selected interface
 function setStaticIP(){
+    # Build interface menu
+    $adapters = Get-NetAdapter | Select ifIndex, Name
+    Write-Host "[*] Displaying list of interfaces available for static IP assignment`n"
+    Write-Host "================"
+    $menu = @{}
+    for ($i=1; $i -le ($adapters | Measure-Object).Count; $i++){
+        Write-Host "$i. $($adapters[$i-1].name)"
+        $menu.Add($i,($adapters[$i-1].ifIndex))
+    }
+    # Prompt user to select interface
+    Write-Host "================`n"
+    [int]$selection = Read-Host "[+] Choose which adapter to update"
+    while ($selection -lt ($adapters | Measure-Object).Count -or $selection -gt ($adapters | Measure-Object).Count){
+        Write-Host "[!] ERROR: Selection is invalid"
+        [int]$selection = Read-Host "[+] Choose which adapter to update"
+    }
+    # Assign requested interface
+    $adapter = $menu.Item($selection)
+
+    # Assign rest of values
     $static = [string]$static
     $gateway = [string]$gateway
     $cidr = "24"
     $dns = "127.0.0.1"
-    $adapter = (Get-NetAdapter).InterfaceIndex
     $ipType = "IPv4"
     $ipRegEx="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+
+    # Validate format of given IP address and gateway
     if ($static -notmatch $ipRegEx){
         Write-Host "[!] ERROR: Static IP address is invalid" -ForegroundColor Red
         exit
@@ -57,10 +77,13 @@ function setStaticIP(){
         Write-Host "[!] ERROR: Gateway IP address is invalid" -ForegroundColor Red
         exit
     }
+    # Remove all information and assign given IP address and gateway
     else{
-        Write-Host "[*] Setting up requested static IP"
-        New-NetIPAddress -IPAddress $static -PrefixLength $cidr -InterfaceIndex $adapter -DefaultGateway $gateway -AddressFamily $ipType | Out-Null
-        Set-DNSClientServerAddress –InterfaceIndex $adapter –ServerAddresses $dns | Out-Null
+        Write-Host "[*] Setting the IP address to $static and gateway to $gateway"
+        Remove-NetIPAddress -InterfaceIndex $adapter -Confirm:$false
+        Remove-NetRoute -InterfaceIndex $adapter -Confirm:$false
+        New-NetIPAddress -IPAddress $static -PrefixLength $cidr -InterfaceIndex $adapter -DefaultGateway $gateway -AddressFamily $ipType -ErrorAction Stop | Out-Null
+        Set-DNSClientServerAddress –InterfaceIndex $adapter –ServerAddresses $dns -ErrorAction Stop | Out-Null
     }
 }
 
@@ -79,6 +102,7 @@ function promoteDC(){
 
     Write-Host ""
     Write-Host "[*] DO NOT CONTINUE UNLESS YOU HAVE TAKEN A SNAPSHOT!"  -ForegroundColor Red
+    Write-Host "[*] The computer will automatically reboot when this is completed"
     Read-Host "Press ENTER to continue..."
 
     <# Sets the DSRM password according to default group policy requirements.
@@ -87,11 +111,11 @@ function promoteDC(){
     #>
     $password = 'P@$$w0rd123' | ConvertTo-SecureString -AsPlainText -Force
 
+    # Add necessary roles and create domain
     Write-Host "[*] Installing necessary roles and features (This may take some time)"
-    Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
-
+    Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools | Out-Null
     Write-Host "[*] Creating domain of $domain.local"
-    Install-ADDSForest -DomainName "$domain.local" -DomainNetBiosName $domain.ToUpper() -InstallDNS -SafeModeAdministratorPassword $password -Force
+    Install-ADDSForest -DomainName "$domain.local" -DomainNetBiosName $domain.ToUpper() -InstallDNS -SafeModeAdministratorPassword $password -Force | Out-Null
 }
 
 
@@ -117,9 +141,6 @@ elseif ([string]::IsNullOrEmpty($domain)){
 }
 else{
     checkAdmin
-    if ($disable){
-        disableServerTools
-    }
     if ($static){
         # TODO: Better error handling for when -static is passed but there is no value
         if ([string]::IsNullOrEmpty($gateway)){
@@ -129,6 +150,9 @@ else{
         else{
             setStaticIP($static, $gateway)
         }
+    }
+    if ($disable){
+        disableServerTools
     }
     promoteDC
 }
